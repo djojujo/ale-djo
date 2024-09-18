590b33779aab24d329bc0092d314fa9a0d3f6318
djojujo <159132703+djojujo@users.noreply.github.com>
2024-09-18 07:42:07 -0300

    hn

---
+++ perguntas.js
@@ -0,0 +1,161 @@
+Criacartao(
+
+    'quimica'
+    
+    'qual o modelo atômico da bola de bilhar?'
+    
+    'Dalton')
+    
+    
+    Criacartao(
+    
+    'quimica'
+    
+    'qual o modelo atômico pudim de passas?'
+    
+    'Thomson')
+    
+    Criacartao (
+    
+    'biologia'
+    
+    "quais os componentes que formam o DNA?'
+    
+    'Adenina, timina, citosina e guanina'
+    
+    Criacartao(
+    
+    'biologia'
+    
+    'Quais os componentes que formam o RNA?'
+    
+    'Adenina, guanina, citosina e uracila')
+    
+    Criacartao (
+    
+    'biologia',
+    
+    'qual a doença transmitida pelo rato?'
+    
+    'leptospirose')
+    
+    Criacartao
+    
+    'historia',
+    
+    'Qual o nome do presidente do Brasil que ficou conhecido como Jango?'
+    
+    'João Goulart')
+    
+    Criacartao (
+    
+    'filosofia',
+    
+    'de quem é a famosa frase, "penso, logo existo"?
+    
+    'Descartes')
+    
+    Criacartao (
+    
+    'geografia',
+    
+    'Qual o menor pais do mundo'?
+    
+    'Vaticano' )
+    
+    Criacartao (
+    
+    'geografia',
+    
+    'Qual o maior país do mundo?'
+    
+    'Russia')
+    
+    Criacartao(
+    
+    'matematica',
+    
+    'qual o valor de PI?'
+    
+    '3,14')
+    
+    Criacartao(
+    
+    'fisica',
+    
+    ' quanto tempo a luz do sol demora para chegar até a terra?'
+    
+    '8 minutos')
+    
+    Criacartao (
+    
+    'historia'',
+    
+    'Em que período da história foi descoberto o fogo?'
+    
+    'paleolítico')
+    
+    Criacartao(
+    
+    'geografia',
+    
+    'qual a montanha mais alta do Brasil?'
+    
+    'Pico da neblina')
+    
+    Criacartao (
+    
+    'matematica',
+    
+    'qual a fórmula do termo geral da P.G?'
+    
+    'an = a1 . qn-1' )
+    
+    Criacartao
+    
+    'matematica',
+    
+    'qual a fórmula geral da P.A?
+    
+    'an = a1 + (n - 1) r' )
+    
+    Criacartao (
+    
+    'fisica'
+    
+    'Qual a fórmula do Movimento retilíneo uniformemente (fórmula do sorvete)?'
+    
+    's = s0 + v . ∆t')
+    
+    
+    Criacartao (
+    
+    'fisica'
+    
+    'qual a fórmula da Energia potencial gravitacional?'
+    
+    'Ep = m . g . h')
+    
+    Criacartao (
+    
+    'fisica',
+    
+    'quanto tempo a terra demora para dar uma volta completa em torno do sol?'
+    
+    '24horas')
+    
+    Criacartao(
+    
+    'quimica',
+    
+    ' qual a fórmula para calcular o número de mols?'
+    
+    'n = m/MM')
+    
+    Criacartao (
+    
+    'quimica',
+    
+    'qual a fórmula para calcular densidade?'
+    
+    ' d = m/V ' )
