b554151128efa91fe4c59381d70e40354a69c4e5
djojujo <159132703+djojujo@users.noreply.github.com>
2024-09-24 08:28:59 -0300

    ,

--- main.js
+++ main.js
@@ -2,6 +2,17 @@ function criacartao ( categoria, pergunta, resposta){
     let container = document.getElementById('container');
     let cartao = document.criateElement('article');
     cartao.classname = 'cartao';
-    cartao.innerHTML = `
+    cartao.innerHTML = 
+    `<div class="cartao-conteudo">
+    <h3>${categoria}</h3>
+    <div class="catao-pergunta">
+        <p>${pergunta}</p>
+    </div>
+    <div class="cartao-respostas">
+            <p>${respostas}</p>
+        </div>
+
+</div>
     `
+    container.appendChild(cartao);
 }
