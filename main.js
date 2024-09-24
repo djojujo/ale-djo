0788205502ffd1dd5cd6f6261de0be0d5d90ed32
djojujo <159132703+djojujo@users.noreply.github.com>
2024-09-24 08:18:37 -0300

    .

---
+++ main.js
@@ -0,0 +1,7 @@
+function criacartao ( categoria, pergunta, resposta){
+    let container = document.getElementById('container');
+    let cartao = document.criateElement('article');
+    cartao.classname = 'cartao';
+    cartao.innerHTML = `
+    `
+}
