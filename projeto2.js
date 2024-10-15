ceb98d46d7e6a8243319b579ac33297b46051b2f
djojujo <159132703+djojujo@users.noreply.github.com>
2024-10-15 08:10:45 -0300

    fghj

---
+++ projeto2.js
@@ -0,0 +1,22 @@
+function Criacartao ( categoria, pergunta, resposta){
+    let container = document.getElementById('container');
+    let cartao = document.createElement('article');
+    cartao.className = 'cartao';
+    cartao.innerHTML = 
+
+    `<div class="cartao-conteudo" onclick="showAnswer(this)"> 
+    <h3>${categoria}</h3>
+    <div class="cartao-pergunta">
+        <p>${pergunta}</p>
+    </div>
+    <div class="cartao-resposta">
+            <p>${resposta}</p>
+        </div>
+</div>
+    `
+    container.appendChild(cartao);
+}
+
+function showAnswer(card){
+
+}
